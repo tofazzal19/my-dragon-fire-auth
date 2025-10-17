56ed6cd01a6eff267a3bbb1be30e53776679d512
tofazzal19 <rana.ict337@gmail.com>
2025-10-17 23:34:06 +0600

    first commit

---
+++ src/components/pages/CategoryNews.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams } from 'react-router';
+
+const CategoryNews = () => {
+
+  const {id} = useParams();
+    console.log(id);
+
+    return <div>CategoryNews - {id}</div>;
+    
+};
+
+export default CategoryNews;
---
+++ src/routes/router.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+import Home from "../components/pages/Home";
+import CategoryNews from "../components/pages/CategoryNews";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomeLayout></HomeLayout>,
+        children: [
+            {
+                path: '',
+                element: <Home></Home>,
+            },
+            {
+                path: '/category/:id',
+                element: <CategoryNews></CategoryNews>,
+            }
+        ]
+    },
+    {
+        path: '/auth',
+        element: <h2>Authentication Layout</h2>,
+    },
+    {
+        path: '/news',
+        element: <h2>News Layout</h2>,
+    },
+    {
+        path: '/*',
+        element: <h2>Error 404</h2>,
+    }
+]);
+
+export default router;
